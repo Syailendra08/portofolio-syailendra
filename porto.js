53ea978143283c560c709574ebbf0197dfb892c0
Syailendra08 <syailendraanggoromukti@smkwikrama.sch.id>
2025-02-27 20:52:55 +0700

    Add files via upload

--- porto.js
+++ porto.js
@@ -19,3 +19,19 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const dropdown = document.querySelector(".dropdown");
+    const dropdownMenu = document.querySelector(".dropdown-menu");
+
+    dropdown.addEventListener("click", function (event) {
+        event.stopPropagation();
+        dropdownMenu.style.display =
+            dropdownMenu.style.display === "block" ? "none" : "block";
+    });
+
+    document.addEventListener("click", function () {
+        dropdownMenu.style.display = "none";
+    });
+});
